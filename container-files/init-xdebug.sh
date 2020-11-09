c72b80ff1233c1473957d737f9a44e76aefe446c
Ernesto Baschny <eb@cron.eu>
2020-11-09 10:05:52 +0100

    XDEBUG_CONFIG is used by xdebug directly
    No need to split by semicolon

--- container-files/init-xdebug.sh
+++ container-files/init-xdebug.sh
@@ -1,32 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-# XDEBUG_CONFIG is either empty (disabled) or contains a list of xdebug settings, separated by semicolon:
-#
-# i.e.:
-#
-#   XDEBUG_CONFIG='idekey=PHPSTORM;remote_enable=1'
-#
-# generates these settings:
-#
-#   xdebug.idekey=PHPSTORM
-#   xdebug.remote_enable=1
+# XDEBUG_CONFIG is either empty (disabled) or contains a list of xdebug settings, separated by spaces.
+# These are used directly by the xdebug module (see https://xdebug.org/docs/remote) and overrides
+# the defaults. I.e.:
+#   XDEBUG_CONFIG='idekey=PHPSTORM remote_enable=1'
 
 if [ -z "${XDEBUG_CONFIG}" ] ||  [ "${XDEBUG_CONFIG}" = "0" ] || [ "${XDEBUG_CONFIG}" = "off" ]
 then
     sed -i -r 's/^zend_extension/;zend_extension/' "$PHP_INI_DIR/conf.d/docker-php-ext-xdebug.ini"
 else
     sed -i -r 's/^;zend_extension/zend_extension/' "$PHP_INI_DIR/conf.d/docker-php-ext-xdebug.ini"
-    if [ "${XDEBUG_CONFIG}" != "on" ] &&  [ "${XDEBUG_CONFIG}" != "1" ]; then
-      IFS=';'
-      for d in $XDEBUG_CONFIG ; do echo "xdebug.$d" >> "$PHP_INI_DIR/conf.d/docker-php-ext-xdebug.ini"; done
-      unset IFS
-    fi
 
-    echo "Xdebug enabled with the follwing config:"
+    echo "Xdebug enabled with the following config:"
     echo "-- 8< --------------------------------------------------"
     cat "$PHP_INI_DIR/conf.d/docker-php-ext-xdebug.ini"
     echo "-- 8< --------------------------------------------------"
+    echo "Overrides in XDEBUG_CONFIG: ${XDEBUG_CONFIG}"
+    echo ""
 
     # create a wrapper script "php-debug" for starting debugging from the CLI
     cat <<'EOF' > /usr/local/bin/php-debug
